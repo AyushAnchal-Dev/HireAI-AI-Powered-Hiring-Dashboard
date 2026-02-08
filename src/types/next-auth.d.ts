a308b2a5f4d6d72883b15989fa412829f8ca3352
AYUSH ANCHAL <abhardwaj8507@gmail.com>
2026-02-08 18:04:33 +0530

    Fixing typescript error

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,10 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       role: string;
-    };
+      id: string; // Ensure id is also typed correctly if used
+    } & DefaultSession["user"];
   }
 }
